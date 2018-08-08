bd8a6120bd8474833a20e3477a2b018ab67955ba
Kendall Wahnschaffe <kendall.wahnschaffe@usaa.com>
2018-08-08 18:09:50 -0500

    fixing mock data

--- src/components/SearchBar/data.js
+++ src/components/SearchBar/data.js
@@ -5,7 +5,7 @@ const data = [
   {label: 'Sodium Methyl Oleoyl Taurate', value: 'sodiumMethylOleoylTaurate', description: 'From Olive Oil.', image: 'https://www.healthline.com/assets/756x0/hlcmsresource/images/AN_images/AN257-Pouring-Olive-Oil-732x549-thumb.jpg.webp'},
   {label: 'Glycol Stearate', value: 'glycolStearate', description: 'From palm kernel.', image: 'https://www.beiersdorf.com/~/media/Beiersdorf/sustainability/products/raw-materials/palm-oil/Palm-Kernels-on-tree-shutterstock.jpg?la=en&mw=940&highRes=1', link: 'https://www.ewg.org/skindeep/ingredient/702701/GLYCOL_STEARATE/#.W2oZlNhKhTY'},
   {label: 'Sodium Chloride', value: 'sodiumChloride', description: 'Salt.', image: 'https://s-i.huffpost.com/gen/2592996/images/n-SALT-628x314.jpg'},
-  {label: 'Sodium Benzoate', value: 'sodiumBenzoate', description: 'A natural preservative from the benzoin tree', image: 'https://hermitageoils.com/wp-content/uploads/2015/06/benzoin-resin-drop.jpg'},
+  {label: 'Sodium Benzoate', value: 'sodiumBenzoate', description: 'A natural preservative from the benzoin tree.', image: 'https://hermitageoils.com/wp-content/uploads/2015/06/benzoin-resin-drop.jpg'},
   {label: 'Citric Acid', value: 'citricAcid', description: 'a natural preservative.', image: 'https://www.pipingrock.com/files/assets/images/Products/Citric-Acid.jpg.pagespeed.ce.Cwbu31wcWA.jpg'}
 ];
 
